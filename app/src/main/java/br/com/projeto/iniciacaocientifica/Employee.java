6e0c3894e4f28398027fa60dce17cb70f7dda461
Gabriel Sanga <gabriel.sanga.fef@gmail.com>
2022-11-20 11:52:08 -0300

    Finalizando comunicação com a APO

---
+++ app/src/main/java/br/com/projeto/iniciacaocientifica/Employee.java
@@ -0,0 +1,14 @@
+package br.com.projeto.iniciacaocientifica;
+
+public class Employee {
+
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
--- app/src/main/java/br/com/projeto/iniciacaocientifica/MainActivity.java
+++ app/src/main/java/br/com/projeto/iniciacaocientifica/MainActivity.java
@@ -5,12 +5,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -20,8 +19,6 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 
-import android.widget.Toast;
-
 import org.jetbrains.annotations.NotNull;
 
 import java.io.ByteArrayOutputStream;
@@ -29,7 +26,6 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
@@ -37,21 +33,34 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageViewFoto;
-    TextView pagenameTextView;
+
+    private MediaPlayer soundClear = null;
+    private MediaPlayer soundNotClear = null;
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private Employee retorno = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        soundClear = MediaPlayer.create(MainActivity.this, R.raw.clear);
+        soundNotClear = MediaPlayer.create(MainActivity.this, R.raw.notclear);
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA }, 0);
         }
 
         imageViewFoto = findViewById(R.id.imageView);
-        pagenameTextView = findViewById(R.id.pagename);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,14 +94,13 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     imagem.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                 }catch(Exception e){
-                    pagenameTextView.setText(e.getMessage());
                     return;
                 }
                 byte[] byteArray = stream.toByteArray();
 
                 RequestBody postBodyImage = new MultipartBody.Builder().setType(MultipartBody.FORM).addFormDataPart("files[]", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray)).build();
 
-                Request request = new Request.Builder().url("http://192.168.0.101:5000/upload").post(postBodyImage).build();
+                Request request = new Request.Builder().url("http://192.168.0.105:5000/upload").post(postBodyImage).build();
 
                 //Enviando requisição e recuperando a resposta
                 client.newCall(request).enqueue(new Callback() {
@@ -101,14 +109,21 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                pagenameTextView.setText(e.getCause().getMessage());
+                                soundNotClear.start();
+                                System.out.println(e);
                             }
                         });
                     }
 
                     @Override
                     public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                        pagenameTextView.setText(response.body().string());
+                        retorno = gson.fromJson(response.body().string(), Employee.class);
+
+                        if (Integer.parseInt(retorno.getResult()) == 1){
+                            soundClear.start();
+                        } else {
+                            soundNotClear.start();
+                        }
                     }
                 });
             }
